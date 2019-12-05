a374e4d36e745cc05da7d95d390318be5e396091
Andre Mori <andremori16@gmail.com>
2019-12-05 14:26:39 +0000

    downloading readme.txt

--- proj2/download.c
+++ proj2/download.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 
 #include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <signal.h>
 #include <stdio.h>
@@ -18,19 +19,34 @@
 #define USER_MSG "user"
 #define PASS_MSG "pass"
 #define PASV_MSG "pasv"
+#define RETR_MSG "retr"
 
 #define NEED_PASS 331
 #define LOGIN_OK 230
 #define PASV_OK 227
 
+// ./download ftp://anonymous:1@ftp.up.pt/pub/CentOS/2.1/readme.txt
+
 typedef struct hostent hostent_t;
+
+typedef struct User {
+    char *username;
+    char *password;
+    char *host;
+    char *url_path;
+    char *filename;
+    int sockfd_client;
+} user_t;
 
 typedef enum cmd
 {
     LOGIN_CMD,
-    PASV_CMD
+    PASV_CMD,
+    RETR_CMD
 } cmd_t;
 
+
+user_t user_info;
 typedef int (*cmd_handler)(int sockfd, va_list args);
 
 hostent_t *getip(char *host, char *readable_addr)
@@ -49,12 +65,20 @@ hostent_t *getip(char *host, char *readable_addr)
     return h;
 }
 
-int parse_user_info(char *username, char *password, char *host, char *url_path, char *info)
+//int parse_user_info(char *username, char *password, char *host, char *url_path, char *info)
+int parse_user_info(char *info)
 {
+    user_info.username = calloc(MAX_STR_SIZE, sizeof(char));
+    user_info.host = calloc(MAX_STR_SIZE, sizeof(char));
+    user_info.password = calloc(MAX_STR_SIZE, sizeof(char));
+    user_info.url_path = calloc(MAX_STR_SIZE, sizeof(char));
+    user_info.filename = calloc(MAX_STR_SIZE, sizeof(char));
+
     // find colon, at and slash characters
     char *colon = strchr(info, ':');
     char *at = strchr(info, '@');
     char *slash = strchr(info, '/');
+    char *last_slash = strrchr(info, '/');
 
     // fail if any of those doesnt exist
     if (colon == NULL || at == NULL || slash == NULL)
@@ -66,12 +90,14 @@ int parse_user_info(char *username, char *password, char *host, char *url_path, 
     unsigned colon_index = colon - info;
     unsigned at_index = at - info;
     unsigned slash_index = slash - info;
+    unsigned last_slash_index = last_slash - info;
 
     // copy username, password, host and path
-    strncpy(username, info, colon_index);
-    strncpy(password, &info[colon_index + 1], at_index - colon_index - 1);
-    strncpy(host, &info[at_index + 1], slash_index - at_index - 1);
-    strncpy(url_path, &info[slash_index + 1], strlen(info) - slash_index - 1);
+    strncpy(user_info.username, info, colon_index);
+    strncpy(user_info.password, &info[colon_index + 1], at_index - colon_index - 1);
+    strncpy(user_info.host, &info[at_index + 1], slash_index - at_index - 1);
+    strncpy(user_info.url_path, &info[slash_index + 1], strlen(info) - slash_index - 1);
+    strncpy(user_info.filename, &info[last_slash_index +1], strlen(info) - last_slash_index - 1);
 
     return 0;
 }
@@ -89,7 +115,7 @@ int get_socket()
     return sockfd;
 }
 
-void server_connect(int sockfd, const char *server_addr)
+void establish_connection(int sockfd, const char *server_addr, int server_port)
 {
     struct sockaddr_in sock_addr;
 
@@ -97,7 +123,7 @@ void server_connect(int sockfd, const char *server_addr)
     bzero((char *)&sock_addr, sizeof(sock_addr));
     sock_addr.sin_family = AF_INET;
     sock_addr.sin_addr.s_addr = inet_addr(server_addr); /*32 bit Internet address network byte ordered*/
-    sock_addr.sin_port = htons(SERVER_PORT);            /*server TCP port must be network byte ordered */
+    sock_addr.sin_port = htons(server_port);            /*server TCP port must be network byte ordered */
 
     /*connect to the server*/
     int connection_status = connect(sockfd, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
@@ -107,6 +133,11 @@ void server_connect(int sockfd, const char *server_addr)
         perror("connect");
         exit(-1);
     }
+}
+
+void server_connect(int sockfd, const char *server_addr)
+{
+    establish_connection(sockfd, server_addr, SERVER_PORT);
 
     char *response = calloc(1024, sizeof(char));
     int bytes_read = 0, total_read = 0;
@@ -161,6 +192,36 @@ char *build_cmd(char *cmd_type, char *cmd_arg)
     printf("> %s\n", command);
 
     return command;
+}
+
+void create_file(int sockfd)
+{
+    int fd = open(user_info.filename, O_WRONLY | O_CREAT, 0666);
+
+    int bytes_written;
+    char buf[1000];
+
+    while((bytes_written = read(user_info.sockfd_client, buf, 1)) > 0)
+        write(fd, buf, 1);
+    
+    puts("> File downloaded succesfully.");
+}
+
+int handle_retr(int sockfd, va_list args)
+{
+    va_end(args);
+
+    char *response = malloc(1024);
+
+    write(sockfd, "retr ", strlen("retr "));
+    write(sockfd, user_info.url_path, strlen(user_info.url_path));
+    write(sockfd, "\n", 1);
+
+    read(sockfd, response, 1024);
+    puts(response);
+    create_file(sockfd);
+
+    return 0;
 }
 
 int handle_login(int sockfd, va_list args)
@@ -221,14 +282,17 @@ int handle_pasv(int sockfd, va_list args)
 
     if (status_code != PASV_OK)
         return -1;
-    
+
     // Get the last two bytes to calculate port number
     char *tok = strtok(strchr(response, '('), ",)");
     int i = 0;
-    while(tok != NULL) {
+    while (tok != NULL)
+    {
         tok = strtok(NULL, ",)");
-        if(i == 3) port = 256 * atoi(tok);
-        else if(i == 4) port += atoi(tok);
+        if (i == 3)
+            port = 256 * atoi(tok);
+        else if (i == 4)
+            port += atoi(tok);
         ++i;
     }
 
@@ -238,7 +302,7 @@ int handle_pasv(int sockfd, va_list args)
     return port;
 }
 
-static cmd_handler handlers[] = {handle_login, handle_pasv};
+static cmd_handler handlers[] = {handle_login, handle_pasv, handle_retr};
 
 int send_cmd(int sockfd, cmd_t cmd, ...)
 {
@@ -256,12 +320,7 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    char *username = calloc(MAX_STR_SIZE, sizeof(char));
-    char *host = calloc(MAX_STR_SIZE, sizeof(char));
-    char *password = calloc(MAX_STR_SIZE, sizeof(char));
-    char *url_path = calloc(MAX_STR_SIZE, sizeof(char));
-
-    if (parse_user_info(username, password, host, url_path, &argv[1][6]) == -1)
+    if (parse_user_info(&argv[1][6]) == -1)
     {
         printf("Usage: ./download ftp://[<username>:<password>@]<host>/<url-path>");
         exit(-1);
@@ -269,23 +328,40 @@ int main(int argc, char *argv[])
 
     char *server_addr = calloc(16, sizeof(char));
 
-    getip(host, server_addr);
+    getip(user_info.host, server_addr);
 
-    printf("username: %s\npassword: %s\nhost: %s\nurl_path: %s\nip_addr: %s\n", username, password, host, url_path, server_addr);
+    printf("username: %s\npassword: %s\nhost: %s\nurl_path: %s\nfilename: %s\nip_addr: %s\n", user_info.username, user_info.password, user_info.host, user_info.url_path, user_info.filename, server_addr);
 
     int sockfd = get_socket();
     server_connect(sockfd, server_addr);
-
-    if (send_cmd(sockfd, LOGIN_CMD, username, password) == -1 ||
-        send_cmd(sockfd, PASV_CMD) == -1)
+    int pasv_port = 0;
+    if (send_cmd(sockfd, LOGIN_CMD, user_info.username, user_info.password) == -1 ||
+        (pasv_port = send_cmd(sockfd, PASV_CMD)) == -1)
     {
         printf("> Invalid login and/or password\n");
+        exit(-1);
+    }
+    
+    if ((user_info.sockfd_client = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket()");
+        exit(0);
     }
 
-    free(username);
-    free(host);
-    free(password);
-    free(url_path);
+    establish_connection(user_info.sockfd_client, server_addr, pasv_port);
+
+
+    if(send_cmd(sockfd, RETR_CMD) == -1)
+    {
+        printf("> Error retrieving file");
+        exit(-1);
+    }
+
+    free(user_info.username);
+    free(user_info.host);
+    free(user_info.password);
+    free(user_info.url_path);
+    free(user_info.filename);
     free(server_addr);
 
     return 0;
